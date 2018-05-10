caf7f7847389fc2d50fb4e0abbe1aa7063d564a5
Cláudio Rocha de Jesus <crochadejesus@gmail.com>
2018-05-10 09:29:41 +0100

    Changed the saving to File.WriteAllBytes.
    The previus version save file with zero bytes

--- Zambo.Utility/FileOperation.cs
+++ Zambo.Utility/FileOperation.cs
@@ -3,31 +3,19 @@ namespace Zambo.Utility
     public class FileOperation
     {
        // http://www.digitalcoding.com/Code-Snippets/C-Sharp/C-Code-Snippet-Save-byte-array-to-file.html       
-       public bool ByteArrayToFile(string path, byte[] bytes)
+       public static void ByteArrayToFile(string path, byte[] bytes)
        {
-         if (string.IsNullOrWhiteSpace(path))
-         {
-            throw new System.ArgumentException("The Path can't be null or white space.", nameof(path));
-         }
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                throw new ArgumentException("The Path can't be null or white space.", nameof(path));
+            }
 
-         if (bytes == null)
-         {
-            throw new System.ArgumentNullException(nameof(bytes));
-         }
+            if (bytes == null)
+            {
+                throw new ArgumentNullException(nameof(bytes));
+            }
 
-         bool response;
-
-             // Open file for reading
-            System.IO.FileStream fileStream = new System.IO.FileStream(path, System.IO.FileMode.Create, System.IO.FileAccess.Write);
-
-            // Writes a block of bytes to this stream using data from a byte array.
-            fileStream.WriteAsync(bytes, 0, bytes.Length);
-            
-            // Close file stream
-            fileStream.Close();
-            response = true;
-
-          return response;
-       }
+            File.WriteAllBytes(path, bytes);
+        }
     }
 }
